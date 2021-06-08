ab4eca5ba6ff952259bd8a91a5669a93f2e7aae9
yeliex wang <yeliex@yeliex.com>
2021-06-08 15:57:27 +0800

    feat: js support inline load

--- src/libs/loader.js
+++ src/libs/loader.js
@@ -1,3 +1,3 @@
 module.exports = (modules) => {
-  return `!function(){var e=window.document,t=e.createElement.bind(e),n=e.body,a=e.head,i=function(e){if(e.length){var s=e.shift();if(s.match(/\\.css(\\?\\w+)?$/)){var d=t("link");d.href=s,d.type="text/css",d.rel="stylesheet",a.appendChild(d),i(e)}else{var r=t("script");r.type="text/javascript",r.src=s;r.onload=function(){i(e)},n.appendChild(r)}}};i(${JSON.stringify(modules)})}();`;
+  return `!function(){var e=window.document,t=e.createElement.bind(e),n=e.body,a=e.head,i=function(e){if(e.length){var s=e.shift();if(s.match(/\\.css(\\?\\w+)?$/)){var d=t("link");d.href=s,d.type="text/css",d.rel="stylesheet",a.appendChild(d),i(e)}else{var r=t("script");r.type="text/javascript",r.src=s;r.onload=function(){i(e)};var q = t('div');q.appendChild(r);e.write(q.innerHTML);}}};i(${JSON.stringify(modules)})}();`;
 };
